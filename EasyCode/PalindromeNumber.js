4e656d04bd9082c8d6ee7e593b6dd3615c2492f0
varunlad <varunlad5@gmail.com>
2025-07-16 02:22:06 +0530

    Solved Palindrome Problem

---
+++ EasyCode/PalindromeNumber.js
@@ -0,0 +1,26 @@
+  //Palindrome Number
+  
+  function isPalindrome(x) {
+
+    if (typeof (x) !== 'number' || x < 0) {
+        return false;
+    }
+    var rev = 0;
+    var num = x;
+    while (num > 0) {
+        rev = rev * 10 + (num % 10);
+        num = parseInt(num / 10);
+    }
+
+    if (rev === x) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    
+};
+
+console.log(isPalindrome(121));
+console.log(isPalindrome(3211));
+console.log(isPalindrome(12321));
