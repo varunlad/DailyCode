1c733b8fcdff6972090ec74b6d41196521d65cf2
varunlad <varunlad5@gmail.com>
2025-07-16 02:56:31 +0530

    Modified Length of Last Word Problem

--- EasyCode/LengthofLastWord.js
+++ EasyCode/LengthofLastWord.js
@@ -1,12 +1,12 @@
 ////Length of Last Word
 
  function lengthOfLastWord(s) {
-    if(s === '') return false;
+    if(s.trim() === '') return 0;
     let wordArr = s.split(" ").filter((ele => ele !== ''));
-    let lastWordLenth =  wordArr[wordArr.length - 1].length;
+    let lastWordLenth =  wordArr[wordArr.length - 1]?.length;
     return lastWordLenth;
 };
-
+console.log(lengthOfLastWord("    "));
 console.log(lengthOfLastWord("Hello World"));
 console.log(lengthOfLastWord("   fly me   to   the moon  "));
 console.log(lengthOfLastWord("luffy is still joyboy"));
