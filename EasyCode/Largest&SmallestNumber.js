cab65bea770f80338be06275c207c5d0e64d2a50
varunlad <varunlad5@gmail.com>
2025-07-27 02:32:34 +0530

    Largest and smallest number in an array

---
+++ EasyCode/Largest&SmallestNumber.js
@@ -0,0 +1,7 @@
+function smallestLargestNumber(arr) {
+  let sortArr = arr.sort((a, b) => a - b);
+  return [sortArr[0], sortArr[sortArr.length - 1]];
+}
+
+
+console.log(smallestLargestNumber([2, 1, 3, 4, 22, 3, 4, 5]));
