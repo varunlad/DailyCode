698fc394c9179cc6324f0c5089cc3373d443e405
Varun Lad <varun.lad@sourcemash.com>
2025-11-11 08:55:17 -0600

    solve the nested array problem

--- ModrateCode/flaten_array.js
+++ ModrateCode/flaten_array.js
@@ -13,5 +13,19 @@ function flaten_array(arr) {
   return result;
 }
 
-let flatArr = flaten_array([1, [2, 3], [4, [5, 6]]]);
+////Alternate Approach
+
+// let flat = [];
+// function flaten_array(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (Array.isArray(arr[i])) {
+//       flat = flaten_array(arr[i]);
+//     } else {
+//       flat.push(arr[i]);
+//     }
+//   }
+//   return flat;
+// }
+
+let flatArr = flaten_array([1, [2, 3], [4, [5, 6, [7, 8]]], 9, [10]]);
 console.log("flatArr = ", flatArr);
--- ModrateCode/max_number_nested_array.js
+++ ModrateCode/max_number_nested_array.js
@@ -1,7 +1,20 @@
-function max_number_nested_array(arr){
-    let max_num = 0;
-    
+let max_num_temp = 0;
+function max_number_nested_array(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flat = max_number_nested_array(arr[i]);
+    } else {
+      if (arr[i] > max_num_temp) {
+        max_num_temp = arr[i];
+      }
+    }
+  }
+  return  max_num_temp;
 }
 
-let max_num = max_number_nested_array([[1,2,3],4,[5,6,[7,8],9]]);
-console.log("max_num = ",max_num);
+let max_num = max_number_nested_array([
+  [1, 2, 3, 10],
+  4,
+  [5, 6, [7, 12, 8], 9],
+]);
+console.log("max_num = ", max_num);
