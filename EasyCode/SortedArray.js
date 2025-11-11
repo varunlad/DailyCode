11889536b33c8f94d9d6b0cdf5725fbbe3d1d4f7
varunlad <varunlad5@gmail.com>
2025-11-12 00:09:34 +0530

    solve sortedArray problem

--- EasyCode/SortedArray.js
+++ EasyCode/SortedArray.js
@@ -1,7 +1,15 @@
-function sortedArray(arr){
-    let sortedArr = [];
-    for(let i=0; i<arr.length; i++){
-
+function sortedArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
     }
+  }
+  return arr
 }
+
+console.log(sortedArray([2, 4, 1, 4, 2, 5, 6, 7, 3, 2, 5]));
 ////Refer Notes
