f2c6fc40263b88355322b52328399c80510fa7dc
varunlad <varunlad5@gmail.com>
2025-11-23 23:15:37 +0530

    modify swap keys to value

--- DSABasics/Object/object_swapKeyValue.js
+++ DSABasics/Object/object_swapKeyValue.js
@@ -3,12 +3,13 @@ let myObject = {
   age: 25,
   city: "bangalore",
 };
+
 function swapKeyValue(obj) {
-  let myVal = Object.values(obj);
-  let myKey = Object.keys(obj);
+  let myVal = Object.values(obj); ///Object property values
+  let myKey = Object.keys(obj);  ///Object property keys
   let newObj = {};
   for (let i = 0; i < myKey.length; i++) {
-    newObj[myVal[i]] = myKey[i];
+    newObj[myVal[i]] = myKey[i];   ////Swap kys with value
   }
   return newObj;
 }
