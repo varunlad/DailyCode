2b007d642d34a41753c1670c8a6ee21c2a285dac
varunlad <varunlad5@gmail.com>
2025-11-25 23:50:22 +0530

    solve arrayToObject problem

---
+++ DSABasics/Array/array_to_object.js
@@ -0,0 +1,8 @@
+function arrayToObject(myArr){
+let myObj = {}
+for(let i=0; i<myArr.length; i++){
+    myObj[myArr[i][0]] = myArr[i][1]
+}
+return myObj;
+}
+console.log(arrayToObject([["name", "Varun"], ["age", 25]]));
